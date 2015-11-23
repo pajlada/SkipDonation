62814dbd85128fafe676334f7be584aa4e372628
Rasmus Karlsson <pajlada@bithack.se>
2015-11-23 18:35:44 +0100

    initial code commit!

---
+++ SkipDonation/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SkipDonation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btn_skip_donation = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_hotkey = new System.Windows.Forms.Label();
+            this.cb_hotkey_enabled = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_skip_donation
+            // 
+            this.btn_skip_donation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_skip_donation.Location = new System.Drawing.Point(12, 32);
+            this.btn_skip_donation.Name = "btn_skip_donation";
+            this.btn_skip_donation.Size = new System.Drawing.Size(268, 229);
+            this.btn_skip_donation.TabIndex = 0;
+            this.btn_skip_donation.Text = "Skip Donation";
+            this.btn_skip_donation.UseVisualStyleBackColor = true;
+            this.btn_skip_donation.Click += new System.EventHandler(this.btn_skip_donation_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current hotkey:";
+            // 
+            // lbl_hotkey
+            // 
+            this.lbl_hotkey.AutoSize = true;
+            this.lbl_hotkey.Location = new System.Drawing.Point(130, 14);
+            this.lbl_hotkey.Name = "lbl_hotkey";
+            this.lbl_hotkey.Size = new System.Drawing.Size(25, 13);
+            this.lbl_hotkey.TabIndex = 3;
+            this.lbl_hotkey.Text = "???";
+            // 
+            // cb_hotkey_enabled
+            // 
+            this.cb_hotkey_enabled.AutoSize = true;
+            this.cb_hotkey_enabled.Checked = true;
+            this.cb_hotkey_enabled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_hotkey_enabled.Location = new System.Drawing.Point(215, 12);
+            this.cb_hotkey_enabled.Name = "cb_hotkey_enabled";
+            this.cb_hotkey_enabled.Size = new System.Drawing.Size(65, 17);
+            this.cb_hotkey_enabled.TabIndex = 4;
+            this.cb_hotkey_enabled.Text = "Enabled";
+            this.cb_hotkey_enabled.UseVisualStyleBackColor = true;
+            this.cb_hotkey_enabled.CheckedChanged += new System.EventHandler(this.cb_hotkey_enabled_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.cb_hotkey_enabled);
+            this.Controls.Add(this.lbl_hotkey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_skip_donation);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "StreamTip TTS Skipper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_skip_donation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_hotkey;
+        private System.Windows.Forms.CheckBox cb_hotkey_enabled;
+    }
+}
+
---
+++ SkipDonation/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SkipDonation;
+
+namespace SkipDonation
+{
+    public partial class MainForm : Form
+    {
+        KeyboardHook hook = new KeyboardHook();
+        DateTime last_run = DateTime.UtcNow.AddSeconds(-10);
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+
+            this.enable_hotkey();
+        }
+
+        void enable_hotkey()
+        {
+            this.set_hotkey("Ctrl+X");
+            // lbl_hotkey.ForeColor = SystemColors.ControlText;
+        }
+
+        void disable_hotkey()
+        {
+            this.hook.Dispose();
+            lbl_hotkey.Text = "Disabled";
+        }
+
+        bool IsBitSet(byte b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+
+        public void set_hotkey(string hotkey_string)
+        {
+            var cvt = new KeysConverter();
+            var key = (Keys)cvt.ConvertFrom(hotkey_string);
+
+            ModifierKeys mk = SkipDonation.ModifierKeys.None;
+
+            if (key.HasFlag(Keys.Control))
+            {
+                mk |= SkipDonation.ModifierKeys.Control;
+                key = key & (~Keys.Control);
+            }
+
+            if (key.HasFlag(Keys.Alt))
+            {
+                mk |= SkipDonation.ModifierKeys.Alt;
+                key = key & (~Keys.Alt);
+            }
+
+            if (key.HasFlag(Keys.Shift))
+            {
+                mk |= SkipDonation.ModifierKeys.Shift;
+                key = key & (~Keys.Shift);
+            }
+
+            hook.RegisterHotKey(mk, key);
+            lbl_hotkey.Text = hotkey_string;
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            var diff = (DateTime.UtcNow - this.last_run).TotalSeconds;
+            if (diff > 1.2)
+            {
+                btn_skip_donation_Click(null, null);
+                this.last_run = DateTime.UtcNow;
+            }
+        }
+
+        private void btn_skip_donation_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("Node2Speech"))
+            {
+                process.Kill();
+            }
+        }
+
+        private void cb_hotkey_enabled_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            if (cb.Checked) {
+                this.enable_hotkey();
+            } else {
+                this.disable_hotkey();
+            }
+        }
+    }
+}
